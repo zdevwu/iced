99b29f98945189a0d3a609ddc564649ac88f8a11
Zhuyuan <zhuyuan.wu@gmail.com>
2021-04-14 21:59:17 +0100

    text color and font options to radio
    (cherry picked from commit 02fc83c02f061ec34e0f2edf45bae646975ed960)

--- native/src/widget/radio.rs
+++ native/src/widget/radio.rs
@@ -1,6 +1,6 @@
 //! Create choices using radio buttons.
 use crate::event::{self, Event};
-use crate::layout;
+use crate::{layout, Color};
 use crate::mouse;
 use crate::row;
 use crate::text;
@@ -46,11 +46,13 @@ pub struct Radio<Message, Renderer: self::Renderer + text::Renderer> {
     size: u16,
     spacing: u16,
     text_size: Option<u16>,
+    text_color: Option<Color>,
+    font: Renderer::Font,
     style: Renderer::Style,
 }
 
 impl<Message, Renderer: self::Renderer + text::Renderer>
-    Radio<Message, Renderer>
+Radio<Message, Renderer>
 where
     Message: Clone,
 {
@@ -80,6 +82,8 @@ where
             size: <Renderer as self::Renderer>::DEFAULT_SIZE,
             spacing: Renderer::DEFAULT_SPACING, //15
             text_size: None,
+            text_color: None,
+            font: Default::default(),
             style: Renderer::Style::default(),
         }
     }
@@ -105,6 +109,18 @@ where
     /// Sets the text size of the [`Radio`] button.
     pub fn text_size(mut self, text_size: u16) -> Self {
         self.text_size = Some(text_size);
+        self
+    }
+
+    /// Sets the text color of the [`Radio`] button.
+    pub fn text_color(mut self, color: Color) -> Self {
+        self.text_color = Some(color);
+        self
+    }
+
+    /// Sets the text font of the [`Radio`] button.
+    pub fn font(mut self, font: Renderer::Font) -> Self {
+        self.font = font;
         self
     }
 
@@ -194,8 +210,8 @@ where
             label_layout.bounds(),
             &self.label,
             self.text_size.unwrap_or(renderer.default_size()),
-            Default::default(),
-            None,
+            self.font,
+            self.text_color.clone(),
             HorizontalAlignment::Left,
             VerticalAlignment::Center,
         );
